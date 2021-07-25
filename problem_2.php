1bedc6b3b7c7871b5f55f3feb355a0b89db5cb1b
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-07-25 18:08:43 +0600

    Second problem solved

---
+++ problem_2.php
@@ -0,0 +1,13 @@
+<!------------ 
+     Declare a variable and set the value “Bangladesh!”.
+     Output:
+          Value that you provided looks like in reverse like this: !hsedalgnaB
+-------------------------------->
+
+<?php
+     //declaring a value
+     $country_name = "Bangladesh!";
+
+     //printing the value
+     echo 'Value that you provided looks like in reverse like this: '.strrev($country_name);
+?>
