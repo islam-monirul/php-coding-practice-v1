934344a28bbbd968ddeafbad485a54559ac33f96
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-07-25 18:48:19 +0600

    8th problem solved

---
+++ problem_8.php
@@ -0,0 +1,19 @@
+<!------------ 
+     Declare a integer value and determine the value is odd or even. If the value is 38
+     Output:
+          38 is even
+-------------------------------->
+<?php
+     // value declaration
+     $number = 39;
+
+     //check gender and print output
+     if($number % 2 == 0){
+          echo $number.' is even';
+     }
+     else{
+          echo $number.' is odd';
+     }
+     
+     
+?>
