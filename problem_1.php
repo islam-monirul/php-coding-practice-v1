6a866f8be13b1ce2a53a9b46ca65fc4e81d0adb5
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-07-25 18:05:48 +0600

    First problem solved

---
+++ problem_1.php
@@ -0,0 +1,13 @@
+<!------------ 
+     Declare a valid variable and insert the value 56.
+     Output:
+          Your value is: 56
+-------------------------------->
+
+<?php
+     //declaring the value
+     $var = 56;
+
+     // printing the value
+     echo 'Your value is: '.$var; 
+?>
