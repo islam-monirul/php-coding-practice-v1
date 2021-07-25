7184f7618bb7e3f21152e7428b31afbb95541a42
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-07-25 18:54:57 +0600

    10th problem solved

---
+++ problem_10.php
@@ -0,0 +1,21 @@
+<!------------ 
+     Using switch case determine if an alphabet vowel or consonant. Vowels are
+     a,e,i,o,u
+     Output:
+          Your input is a vowel
+          OR
+          Your input is a consonant
+-------------------------------->
+<?php
+     //declaring iteration variable
+     $alphabet = 'i';
+
+     //find vowel or not
+     if($alphabet === 'a' || $alphabet === 'e' || $alphabet === 'i' || $alphabet === 'o' || $alphabet === 'u'){
+          echo 'Your input is a vowel';
+     }
+     else{
+          echo 'Your input is a consonant';
+     }
+     
+?>
