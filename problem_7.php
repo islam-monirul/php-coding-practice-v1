66f1d11a72ebb6bfa0124f0cda2731892b653771
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-07-25 18:45:25 +0600

    7th problem solved

---
+++ problem_7.php
@@ -0,0 +1,23 @@
+<!------------ 
+     Declare a variable called gender and assign a value. If gender is male output a
+     string or if female then out another string.
+     Output:
+          You are a male
+          OR
+          You are a female
+
+-------------------------------->
+<?php
+     // value declaration
+     $gender = 'male';
+
+     //check gender and print output
+     if($gender === 'male'){
+          echo 'You are a male';
+     }
+     else{
+          echo 'You are a female';
+     }
+     
+     
+?>
