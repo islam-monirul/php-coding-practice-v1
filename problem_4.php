b2a554fb9665521a6f8329624b0ce0a6550ae9ad
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-07-25 18:33:23 +0600

    4th problem solved

---
+++ problem_4.php
@@ -0,0 +1,14 @@
+<!------------ 
+     Declare a constant value of 3.1416.
+     Output:
+          Constant value of PI is 3.1416
+-------------------------------->
+
+<?php
+     // declaring constant variable
+     define('pi', '3.1416');
+
+     //printing output
+     echo 'Constant value of PI is '.pi;
+     
+?>
