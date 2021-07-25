58023a29636b9d98e7b89d6d2b05365a24bb7987
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-07-25 18:38:42 +0600

    5th problem solved

---
+++ problem_5.php
@@ -0,0 +1,19 @@
+<!------------ 
+     Declare two integer variables with values of 54, 38. Use arithmetic operation.
+     Output:
+          Addition of two values: (92)
+          Division of two values: 1.4210526315789
+          Subtraction of two values: $16
+          Multiplication of two values: #2052#
+-------------------------------->
+<?php
+     // declaring values
+     $x = 54;
+     $y = 38;
+
+     //arithmetic calculations and print output
+     echo 'Addition of two values: '.($x + $y).'<br>';
+     echo 'Division of two values: '.($x / $y).'<br>';
+     echo 'Subtraction of two values: '.'$'.($x - $y).'<br>';
+     echo 'Multiplication of two values: '.'#'.($x * $y).'#';
+?>
