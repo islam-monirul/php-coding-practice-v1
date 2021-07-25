ece28ca35368021ae378ffa707a22f09e77164c1
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-07-25 18:51:15 +0600

    9th problem solved

---
+++ problem_9.php
@@ -0,0 +1,15 @@
+<!------------ 
+     Using while loop print 1 to 10
+     Output:
+          1#2#3#4#5#6#7#8#9#10#
+-------------------------------->
+<?php
+     //declaring iteration variable
+     $i = 1;
+
+     //print 1 to 10 using while loop
+     while($i <= 10){
+          echo $i.'#';
+          $i++;
+     }
+?>
